df36fd174e5fedc770ef482bcf01a74c095d4dcf
Clement Fournier <clemfournier@outlook.com>
2023-05-24 13:40:17 -0700

    update program

--- claimapp/programs/claimapp/src/lib.rs
+++ claimapp/programs/claimapp/src/lib.rs
@@ -1,13 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_lang::Accounts;
-use anchor_lang::solana_program::program::invoke_signed;
-use anchor_lang::solana_program::entrypoint::ProgramResult;
-use solana_program::{
-    account_info::AccountInfo,
-    entrypoint,
-    entrypoint::ProgramResult,
-    pubkey::Pubkey,
-};
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("79GRqHSs8MPinewuYuaSyQgTYD1WruUguy8JvRdMLe7T");
 
@@ -15,23 +7,26 @@ declare_id!("79GRqHSs8MPinewuYuaSyQgTYD1WruUguy8JvRdMLe7T");
 pub mod claimapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        let seeds: &[&[&[u8]]] = &[&[&[b'p', b'r', b'e', b'f', b'i', b'x'], &[1u8]]];
+    pub fn initialize(ctx: Context<Initialize>, x_amount: u64, y_amount: u64) -> Result<()> {
+        let escrow = &mut ctx.accounts.escrow;
+        escrow.bump = *ctx.bumps.get("escrow").unwrap();
+        escrow.authority = ctx.accounts.seller.key();
+        escrow.escrowed_x_tokens = ctx.accounts.escrowed_x_tokens.key();
+        escrow.y_amount = y_amount; // number of token sellers wants in exchange
+        escrow.y_mint = ctx.accounts.y_mint.key(); // token seller wants in exchange
 
-        let ix = spl_token::instruction::initialize_account(
-            &spl_token::ID,
-            &ctx.accounts.bonk_escrow.to_account_info().key,
-            &ctx.accounts.bonk_mint.to_account_info().key,
-            &ctx.accounts.authority.to_account_info().key,
+        // Transfer seller's x_token in program owned escrow token account
+        anchor_spl::token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::Transfer {
+                    from: ctx.accounts.seller_x_token.to_account_info(),
+                    to: ctx.accounts.escrowed_x_tokens.to_account_info(),
+                    authority: ctx.accounts.seller.to_account_info(),
+                },
+            ),
+            x_amount,
         )?;
-
-        invoke_signed(&ix, &[
-            ctx.accounts.bonk_escrow.clone(),
-            ctx.accounts.bonk_mint.clone(),
-            ctx.accounts.authority.clone(),
-            ctx.accounts.token_program.clone(),
-            ctx.accounts.system_program.clone(),
-        ], seeds);
 
         Ok(())
     }
@@ -39,22 +34,51 @@ pub mod claimapp {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(signer)]
-    authority: AccountInfo<'info>,
-    bonk_escrow: AccountInfo<'info>,
-    bonk_mint: AccountInfo<'info>,
-    #[account("spl_token::ID")]
-    token_program: AccountInfo<'info>,
-    #[account("solana_system_program::ID")]
-    system_program: AccountInfo<'info>,
+
+    /// `seller`, who is willing to sell his token_x for token_y
+    #[account(mut)]
+    seller: Signer<'info>,
+
+    /// Token x mint for ex. USDC
+    x_mint: Account<'info, Mint>,
+    /// Token y mint 
+    y_mint: Account<'info, Mint>,
+
+    /// ATA of x_mint 
+    #[account(mut, constraint = seller_x_token.mint == x_mint.key() && seller_x_token.owner == seller.key())] 
+    seller_x_token: Account<'info, TokenAccount>,
+
+    #[account(
+        init, 
+        payer = seller,  
+        space=Escrow::LEN,
+        seeds = ["escrow6".as_bytes(), seller.key().as_ref()],
+        bump,
+    )]
+    pub escrow: Account<'info, Escrow>,
+
+    #[account(
+        init,
+        payer = seller,
+        token::mint = x_mint,
+        token::authority = escrow,
+    )]
+    escrowed_x_tokens: Account<'info, TokenAccount>,
+
+    token_program: Program<'info, Token>,
+    rent: Sysvar<'info, Rent>,
+    system_program: Program<'info, System>,
 }
 
-#[derive(Accounts)]
-pub struct Airdrop<'info> {
-    #[account(signer)]
-    authority: AccountInfo<'info>,
-    bonk_escrow: AccountInfo<'info>,
-    to: AccountInfo<'info>,
-    #[account("spl_token::ID")]
-    token_program: AccountInfo<'info>,
+#[account]
+pub struct Escrow {
+    authority: Pubkey,
+    bump: u8,
+    escrowed_x_tokens: Pubkey,
+    y_mint: Pubkey,
+    y_amount: u64,
+}
+
+impl Escrow {
+    pub const LEN: usize = 8 + 1+ 32 + 32 + 32 + 8;
 }
